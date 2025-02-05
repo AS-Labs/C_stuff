2e545e4e0ff89d5639147fc180c236a45260218a
as-labs <a.alshaiji@protonmail.com>
2025-02-05 21:41:18 +0300

    update

--- HMH/test.cpp
+++ HMH/test.cpp
@@ -1,13 +1,30 @@
-
 #include <stdio.h>
 
 
 
-int main(){
-	unsigned char Test;
+int main()
+{
+//	char sSmall; // 8 bits - 256 values, negative and positive values, [-128,127]
+//	char unsigned uSmall; // 8 bits positive, [0,255]
+//	
+//	
+//	short sMedium; // 16 bits - 65536 values
+//	short unsigned uMedium;
+//	
+//	
+//	int sLarge; // 32 bits - 4 billion
+//	int unsigned uLarge; // 32 bits - 4 billion
+	
+	int Test = 0;
+	int *pTest;
+	pTest = &Test;
 
-	Test = 200;
-	printf("Test=%d", Test);
+	// basically copy 200 to Test
+	Test = 255;
+	pTest = &Test;
+	printf("1. pTest=%d\n", *pTest);
+	pTest = &Test + 1;
+	printf("Test=%d\n", Test);
+	printf("2. pTest=%d\n", *pTest);
 	return 0;
-
 }
---
+++ calc_day3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 4){
+        printf("Usage: %s n1 op n2\n", argv[0]);
+    }
+
+    char addition = '+';
+    if (argv[2] == addition)
+    printf("Calculating");
+
+
+
+}
